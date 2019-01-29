41d311f4fe198c91790e1c8fd2d2164841d446b2
Gangana3 <organy110@gmail.com>
2019-01-29 12:45:04 +0200

    Adding initial GUI files

---
+++ Organic Browser/Organic Browser/Controls/UrlTextBoxControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Organic_Browser.Controls
+{
+    /// <summary>
+    /// Interaction logic for UrlTextBoxControl.xaml
+    /// </summary>
+    public partial class UrlTextBoxControl : UserControl
+    {
+        public UrlTextBoxControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Organic Browser/Organic Browser/Utils/CefInitializer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organic_Browser.Utils
+{
+    /// <summary>
+    /// This class represents a Chromium Embedded Browser initializer
+    /// </summary>
+    class CefInitializer
+    {
+        // Private attributes
+        private static readonly Dictionary<string, string> Commands;
+
+        static CefInitializer()
+        {
+            // All the commands for the CefSettingsCommandLine
+            Commands = new Dictionary<string, string>
+            {
+                // Improve performence on high resolutions
+                {"disable-gpu-vsync", "1"},     
+                {"disable-gpu", "1"}
+            };
+        }
+
+        public static void Initialize()
+        {
+            // Add the settings to the CefSettings object
+            var settings = new CefSharp.Wpf.CefSettings();
+            foreach (KeyValuePair<string, string> command in Commands)
+            {
+                settings.CefCommandLineArgs.Add(command);
+            }
+
+            // Initialize Cef
+            CefSharp.Cef.Initialize(settings);
+        }
+    }
+}
