e5b0f8ff28c7972b80f7b4ea9e47bdf437046311
Gangana3 <organy110@gmail.com>
2019-02-01 11:20:36 +0200

    Adding new UserSettings.cs

---
+++ Organic Browser/Organic Browser/Utils/UserSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace Organic_Browser.Utils
+{
+    /// <summary>
+    /// Represents the user settings
+    /// </summary>
+    class UserSettings
+    {
+        
+    }
+}
