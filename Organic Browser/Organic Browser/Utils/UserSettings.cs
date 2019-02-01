7b0da6edcfcb02d7e87f23995d757f0bfedce897
Gangana3 <organy110@gmail.com>
2019-02-01 12:10:47 +0200

    Adding implementation for usersettings class

--- Organic Browser/Organic Browser/Utils/UserSettings.cs
+++ Organic Browser/Organic Browser/Utils/UserSettings.cs
@@ -9,11 +9,88 @@ using System.Runtime.Serialization.Json;
 
 namespace Organic_Browser.Utils
 {
-    /// <summary>
-    /// Represents the user settings
-    /// </summary>
-    class UserSettings
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.IO;
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Json;
+
+    namespace ConsoleApp29
     {
-        
+        /// <summary>
+        /// Contains all the user settings such as home page, and downloaded webpages location
+        /// 
+        /// (Implements Singleton design pattern)
+        /// </summary>
+        [DataContract]
+        class UserSettings
+        {
+            private static UserSettings Instance = null;    // Single instance
+
+            // Private readonly attributes
+            private const string JsonPath = "settings.json";                                                                                // Path to the json file
+            private const string DefaultHomePage = "www.google.com";                                                                        // Default path to home page
+            private static readonly string DefaultDownloadPagesLocation = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);     // Default location to download webpages
+
+            // Public properties
+            [DataMember] public string HomePage { get; set; }                       // Home page name
+            [DataMember] public string DownloadWebpagesLocation { get; set; }       // Downloaded webpage location
+
+            // Private constructor
+            private UserSettings() { }
+
+            /// <summary>
+            /// Saves the settings to the json file
+            /// </summary>
+            public void Save()
+            {
+                Stream stream = new StreamWriter(JsonPath).BaseStream;
+                var serializer = new DataContractJsonSerializer(typeof(UserSettings));
+                serializer.WriteObject(stream, this);
+                stream.Dispose();
+            }
+
+            /// <summary>
+            /// Loads the User settings from the json file (if exists)
+            /// </summary>
+            /// <returns></returns>
+            public static UserSettings Load()
+            {
+                // In case settings file was not loaded yet
+                if (Instance == null)
+                {
+                    // In case file exists
+                    if (File.Exists(JsonPath))
+                    {
+                        var stream = new StreamReader(JsonPath).BaseStream;
+                        var serializer = new DataContractJsonSerializer(typeof(UserSettings));
+                        UserSettings result = (UserSettings)serializer.ReadObject(stream);
+                        stream.Dispose();
+                        return result;
+                    }
+                    // In case file does NOT exist
+                    else
+                    {
+                        // Return a result with default settings
+                        var result = new UserSettings
+                        {
+                            HomePage = "www.google.com",
+                            DownloadWebpagesLocation = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                        };
+                        result.Save();
+                        return result;
+                    }
+                }
+                // In case settings file was already loaded
+                else
+                {
+                    return Instance;
+                }
+            }
+        }
     }
+
 }
